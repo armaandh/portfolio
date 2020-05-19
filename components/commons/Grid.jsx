bd0c73247cc31dcb371a49d373237799549cc230
Jean-Claude Pratt-Delzenne <prattjames4@gmail.com>
2020-05-19 16:11:20 +0200

    feature: experiments gallery image

--- components/commons/Grid.jsx
+++ components/commons/Grid.jsx
@@ -30,90 +30,105 @@ class Grid extends Component {
       >
         <div className="grid-item" key="key1">
           <Image
+            grid
             title="Calendar software"
             src="/experiments/calendar-app.png"
           />
         </div>
         <div className="grid-item" key="key2">
-          <Image 
+          <Image
+            grid
             title="Blood donation app"
             src="/experiments/save-mockup.png"
           />
         </div>
         <div className="grid-item" key="key9">
           <Image
+            grid
             title="Meditation app"
             src="/experiments/meditation-app.png"
           />
         </div>
         <div className="grid-item" key="key8">
           <Image
+            grid
             title="Search micro-interaction"
-            src="experiments/search-1.gif"
+            src="/experiments/search-1.gif"
           />
         </div>
         <div className="grid-item" key="key7">
           <Image
+            grid
             title="Flower pots e-commerce app"
             src="/experiments/flower-plant-app.png"
           />
         </div>
         <div className="grid-item" key="key10">
           <Image 
+            grid
             title="Insurance management software"
             src="/experiments/insurance-app-web.png"
           />
         </div>
         <div className="grid-item" key="key6">
           <Image 
+            grid
             title="Finance management software"
             src="/experiments/banking-1.png"
           />
         </div>
         <div className="grid-item" key="key5">
-          <Image 
+          <Image
+            grid
             title="Finance management app"
             src="/experiments/financial-app-1.png"
           />
         </div>
         <div className="grid-item" key="key3">
           <Image
+            grid
             title="Finance management app"
             src="/experiments/insurance-credit-app.png"
           />
         </div>
         <div className="grid-item" key="key4">
           <Image
+            grid
             title="Project management software components"
             src="/experiments/components-1.png"
           />
         </div>
         <div className="grid-item" key="key14">
           <Image
+            grid
             title="Chart component"
             src="/experiments/chart-1.png"
           />
         </div>
         <div className="grid-item" key="key13">
-          <Image 
+          <Image
+            grid
             title="Calendar component"
             src="/experiments/calendar.png"
           />
         </div>
         <div className="grid-item" key="key15">
           <Image
+            grid
             title="Bar chart component"
             src="/experiments/bar-1.png"
           />
         </div>
         <div className="grid-item" key="key12">
           <Image
+            grid
             title="Menu micro-interaction"
             src="/experiments/menu-1.gif"
           />
         </div>
         <div className="grid-item" key="key16">
           <Image
+            grid
             title="Round chart component"
             src="/experiments/round-1.png"
           />
--- components/commons/Image.jsx
+++ components/commons/Image.jsx
@@ -3,10 +3,21 @@ import Lightbox from 'react-image-lightbox';
 import { Item } from '../Boxes';
 
 const images = [
-  '//placekitten.com/1500/500',
-  '//placekitten.com/4000/3000',
-  '//placekitten.com/800/1200',
-  '//placekitten.com/1500/1500',
+  "/experiments/calendar-app.png",
+  "/experiments/save-mockup.png",
+  "/experiments/meditation-app.png",
+  "/experiments/search-1.gif",
+  "/experiments/flower-plant-app.png",
+  "/experiments/insurance-app-web.png",
+  "/experiments/banking-1.png",
+  "/experiments/financial-app-1.png",
+  "/experiments/insurance-credit-app.png",
+  "/experiments/components-1.png",
+  "/experiments/chart-1.png",
+  "/experiments/calendar.png",
+  "/experiments/bar-1.png",
+  "/experiments/menu-1.gif",
+  "/experiments/round-1.png",
 ];
 
 export default class Image extends Component {
@@ -15,30 +26,41 @@ export default class Image extends Component {
 
     this.state = {
       isOpen: false,
+      photoIndex: props.grid ? images.indexOf(props.src) : 0,
     };
   }
 
   render() {
-    const { isOpen } = this.state;
+    const { isOpen, photoIndex } = this.state
+    const { grid, src, title, width, height, productImage, className , mb } = this.props
+    const mainSrc = grid ? images[photoIndex] : src
+    const nextSrc = grid ? images[(photoIndex + 1) % images.length] : undefined
+    const prevSrc = grid ? images[(photoIndex + images.length - 1) % images.length] : undefined
+    const onMovePrevRequest = grid ? () => this.setState({ photoIndex: (photoIndex + images.length - 1) % images.length, }) : undefined
+    const onMoveNextRequest = grid ?  () => this.setState({ photoIndex: (photoIndex + 1) % images.length, }) : undefined
     return (
       <>
         <Item
-          src={this.props.src}
+          src={src}
           onClick={() => this.setState({ isOpen: true })}
-          width={this.props.width}
-          height={this.props.height}
-          mb={this.props.mb}
-          productImage={this.props.productImage}
-          className={this.props.className}
+          width={width}
+          height={height}
+          mb={mb}
+          productImage={productImage}
+          className={className}
         />
         {isOpen && (
           <Lightbox
-            imageTitle={this.props.title}
+            imageTitle={title}
             reactModalProps={{
               bodyOpenClassName: "overflow"
             }}
-            mainSrc={this.props.src}
             onCloseRequest={() => this.setState({ isOpen: false })}
+            mainSrc={mainSrc}
+            nextSrc={nextSrc}
+            prevSrc={prevSrc}
+            onMovePrevRequest={onMovePrevRequest}
+            onMoveNextRequest={onMoveNextRequest}
           />
         )}
       </>
